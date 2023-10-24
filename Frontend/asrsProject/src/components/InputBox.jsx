bf02509993ed0b9ad2bd9c97c131cab7c5a83452
afeefuddin <afeefud2004din@gmail.com>
2023-10-25 00:17:39 +0530

    Changes in Dictionary Page

--- Frontend/asrsProject/src/components/InputBox.jsx
+++ Frontend/asrsProject/src/components/InputBox.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
-
+import { useState } from 'react';
 import styles from './InputBox.module.css'
 import Button from './Button';
-const{pageContainer,container,searchox,textContainer,text} = styles;
-export default function InputBox({onButtonClick}) {
+const{pageContainer,container,searchox,textContainer,SearchBox,SearchBoxInput,buttonContainer} = styles;
+export default function InputBox(props) {
+  const [inputValue, setInputValue] = useState("");
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+    // Update the searchbox state in the parent component
+    props.onChange(e.target.value);
+  };
   return (
     <div className={pageContainer}>
         <div className={container}>
         <div className={searchox}>
             <div className={textContainer}>Find the Real Meaning of Words</div>
-            <input type={text} />
+            <div className={SearchBox}>
+              <input type="text" value={inputValue} className={SearchBoxInput} onChange={handleInputChange} />
+            </div>
+            
         </div>
-        <Button onClick={onButtonClick} content='Search'></Button>
+        <div className={buttonContainer}>
+        <Button onClick={props.onButtonClick} content='Search'></Button>
+        </div>
         </div>
     </div>
   )
--- Frontend/asrsProject/src/pages/RealDictionary.jsx
+++ Frontend/asrsProject/src/pages/RealDictionary.jsx
@@ -1,20 +1,35 @@
 import { useState } from "react";
 import InputBox from "../components/InputBox";
-
+import Navbar from "../components/Navbar";
+import styles from "./RealDictionary.module.css"
+const { searchboxStyle } = styles;
 function RealDictionary(){
     const [searchbox,changesearch] = useState("")
     const [searched,setSearched] = useState(false);
     const handleButtonClick = () =>{
         setSearched(true);
+
     }
+    const handleSearchvalue=(newvalue)=>{
+        changesearch(newvalue)
+    }
+if(!searched){
+    return (
+        <>
+           <Navbar />
+           <InputBox onButtonClick={handleButtonClick} onChange={handleSearchvalue}/>
+              
+       
+       </>
+    )
+}
+
 return(
     <>
-    
-     {
-        !searched && 
-        <InputBox onButtonClick={handleButtonClick}/>
-     }   
-    
+    <h1>ASRS</h1>
+    <div className="flex justify-center items-center h-16">
+    <input className={searchboxStyle} value={searchbox} />
+    </div>
     </>
 );
 }
