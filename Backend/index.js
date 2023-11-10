6c4e39c78aa73cfb20f4db5a57c4bc149ac28531
afeefuddin <afeefud2004din@gmail.com>
2023-11-10 19:18:38 +0530

    Fix

--- Backend/index.js
+++ Backend/index.js
@@ -3,10 +3,10 @@ let app = express();
 let cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
-const {getMeaning} = require('../Model/api/dictionary')
-const findMeaning = require('../Model/database/Meaning');
-const DB = require('../Model/database/connectDB');
-const router = require('../Routes/route');
+const {getMeaning} = require('./Model/api/dictionary')
+const findMeaning = require('./Model/database/Meaning');
+const DB = require('./Model/database/connectDB');
+const router = require('./Routes/route');
 
 app.use(cors({
     origin: "*"
@@ -14,8 +14,10 @@ app.use(cors({
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// app.use('/',(req,res)=>{
+//     res.json({message : "HELLO"})
+// })
 app.use(router);
 DB();
-
 let port = 8000;
 app.listen(port,console.log(`Started the server on port ${port}`))
